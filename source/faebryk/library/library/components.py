e29d1f9667c12f07664d45df5210de3e6bb5d818
Ioannis P <ioannis@iteng.nl>
2023-01-05 17:09:30 +0100

    Base schematic

--- source/faebryk/library/library/components.py
+++ source/faebryk/library/library/components.py
@@ -1,3 +1,171 @@
+from enum import Enum
 import logging
 
 logger = logging.getLogger("local_library")
+
+from faebryk.library.core import Component, Interface
+from faebryk.library.trait_impl.component import has_defined_type_description, can_bridge_defined, has_defined_footprint, has_defined_footprint_pinmap
+from faebryk.library.library.interfaces import Electrical, Power
+from faebryk.library.library.components import Resistor, LED
+from faebryk.library.library.parameters import TBD
+from faebryk.library.util import times
+from faebryk.library.kicad import KicadFootprint
+
+
+class MOSFET(Component):
+    class ChannelType(Enum):
+        N_CHANNEL = 1
+        P_CHANNEL = 2
+
+    class SaturationType(Enum):
+        ENHANCEMENT = 1
+        DEPLETION = 2
+
+    def __new__(cls, *args, **kwargs):
+        self = super().__new__(cls)
+        self._setup_traits()
+        return self
+
+    def __init__(
+        self, channel_type: ChannelType, saturation_type: SaturationType
+    ) -> None:
+        super().__init__()
+        self._setup_interfaces()
+
+    def _setup_traits(self):
+        self.add_trait(has_defined_type_description("MOSFET"))
+
+    def _setup_interfaces(self):
+        class _IFs(Component.InterfacesCls()):
+            source = Electrical()
+            gate = Electrical()
+            drain = Electrical()
+
+        self.IFs = _IFs(self)
+        self.add_trait(can_bridge_defined(self.IFs.source, self.IFs.drain))
+
+
+class PowerSwitch(Component):
+    def __init__(self, lowside: bool, normally_closed: bool) -> None:
+        super().__init__()  # interfaces
+
+        class _IFs(Component.InterfacesCls()):
+            #TODO replace with logical
+            logic_in = Electrical()
+            power_in = Power()
+            switched_power_out = Power()
+
+        self.IFs = _IFs(self)
+
+        # components
+        class _CMPs(Component.ComponentsCls()):
+            mosfet = MOSFET(
+                MOSFET.ChannelType.N_CHANNEL
+                if lowside
+                else MOSFET.ChannelType.P_CHANNEL,
+                MOSFET.SaturationType.ENHANCEMENT,
+            )
+            pull_resistor = Resistor(TBD)
+
+        self.CMPs = _CMPs(self)
+
+        # pull gate
+        self.CMPs.mosfet.IFs.gate.connect_via(
+            self.CMPs.pull_resistor,
+            self.IFs.power_in.IFs.lv
+            if lowside and not normally_closed
+            else self.IFs.power_in.IFs.hv,
+        )
+
+        # passthrough non-switched side, bridge switched side
+        if lowside:
+            self.IFs.power_in.IFs.hv.connect(self.IFs.switched_power_out.IFs.hv)
+            self.IFs.power_in.IFs.lv.connect_via(
+                self.CMPs.mosfet, self.IFs.switched_power_out.IFs.lv
+            )
+        else:
+            self.IFs.power_in.IFs.lv.connect(self.IFs.switched_power_out.IFs.lv)
+            self.IFs.power_in.IFs.hv.connect_via(
+                self.CMPs.mosfet, self.IFs.switched_power_out.IFs.hv
+            )
+
+        # Add bridge trait
+        self.add_trait(can_bridge_defined(self.IFs.power_in, self.IFs.switched_power_out))
+
+class PoweredLED(Component):
+    def __init__(self) -> None:
+        super().__init__()
+
+        class _IFs(Component.InterfacesCls()):
+            power = Power()
+
+        self.IFs = _IFs(self)
+
+        class _CMPs(Component.ComponentsCls()):
+            current_limiting_resistor = Resistor(TBD)
+            led = LED()
+
+        self.CMPs = _CMPs(self)
+
+        self.IFs.power.IFs.hv.connect(self.CMPs.led.IFs.anode)
+        self.IFs.power.IFs.lv.connect_via(self.CMPs.current_limiting_resistor, self.CMPs.led.IFs.cathode)
+
+#TODO
+class Logical(Interface):
+    def __init__(self, high_ref: Electrical, low_ref: Electrical) -> None:
+        super().__init__()
+
+class DifferentialPair(Interface):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        class _IFs(Interface.InterfacesCls()):
+            p = Electrical()
+            n = Electrical()
+
+        self.IFs = _IFs(self)
+
+    def connect(self, other: Interface) -> Interface:
+        assert type(other) is DifferentialPair, "can't connect to different type"
+        for s, d in zip(self.IFs.get_all(), other.IFs.get_all()):
+            s.connect(d)
+
+        return self
+
+
+class USB_C_Receptacle(Component):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # interfaces
+        class _IFs(Component.InterfacesCls()):
+            cc1 = Electrical()
+            cc2 = Electrical()
+            sbu1 = Electrical()
+            sbu2 = Electrical()
+            shield = Electrical()
+            # power
+            gnd = times(4, Electrical)
+            vbus = times(4, Electrical)
+            # diffpairs: p, n
+            rx1 = DifferentialPair()
+            rx2 = DifferentialPair()
+            tx1 = DifferentialPair()
+            tx2 = DifferentialPair()
+            d1 = DifferentialPair()
+            d2 = DifferentialPair()
+
+        self.IFs = _IFs(self)
+
+        #TODO
+        #self.add_trait(has_defined_footprint(KicadFootprint()))
+
+class RJ45_Receptacle(Component):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # interfaces
+        class _IFs(Component.InterfacesCls()):
+            twisted_pairs = times(4, DifferentialPair)
+
+        self.IFs = _IFs(self)
--- source/faebryk/main.py
+++ source/faebryk/main.py
@@ -7,6 +7,7 @@ TODO: Explain file
 
 import logging
 from pathlib import Path
+from typing import List
 
 logger = logging.getLogger("main")
 
@@ -16,17 +17,30 @@ from faebryk.exporters.netlist.graph import (
     make_t1_netlist_from_graph,
 )
 
+
 # function imports
 from faebryk.exporters.netlist.kicad.netlist_kicad import from_faebryk_t2_netlist
 from faebryk.exporters.netlist.netlist import make_t2_netlist_from_t1
 
 # library imports
 from faebryk.library.core import Component
+from faebryk.library.library.interfaces import Electrical, Power
+from faebryk.library.library.components import Resistor
+from faebryk.library.library.parameters import Constant
 from faebryk.library.trait_impl.component import has_symmetric_footprint_pinmap
 from faebryk.library.traits.component import has_footprint
 
+from faebryk.library.util import times
+
 # Project library imports
-# from library.library.components import ()
+from library.library.components import (
+    MOSFET,
+    DifferentialPair,
+    PowerSwitch,
+    PoweredLED,
+    RJ45_Receptacle,
+    USB_C_Receptacle,
+)
 
 K = 1000
 M = 1000_000
@@ -34,6 +48,166 @@ G = 1000_000_000
 
 n = 0.001 * 0.001 * 0.001
 u = 0.001 * 0.001
+
+
+class USB_C_PSU(Component):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # interfaces
+        class _IFs(Component.InterfacesCls()):
+            power_out = Power()
+
+        self.IFs = _IFs(self)
+
+        # components
+        class _CMPs(Component.ComponentsCls()):
+            usb = USB_C_Receptacle()
+            configuration_resistors = times(2, lambda: Resistor(Constant(5.1*K)))
+
+        self.CMPs = _CMPs(self)
+
+        self.IFs.power_out.IFs.hv.connect_all(self.CMPs.usb.IFs.vbus)
+        self.IFs.power_out.IFs.lv.connect_all(self.CMPs.usb.IFs.gnd)
+
+        # configure as ufp with 5V@max3A
+        self.CMPs.usb.IFs.cc1.connect_via(self.CMPs.configuration_resistors[0], self.IFs.power_out.IFs.lv)
+        self.CMPs.usb.IFs.cc2.connect_via(self.CMPs.configuration_resistors[1], self.IFs.power_out.IFs.lv)
+
+class LEDIndicator(Component):
+    def __init__(self, logic_low: bool, normally_on: bool) -> None:
+        super().__init__()
+
+        # interfaces
+        class _IFs(Component.InterfacesCls()):
+            # hv = high #TODO replace with Logical
+            logic_in = Electrical()
+            power_in = Power()
+
+        self.IFs = _IFs(self)
+
+        # components
+        class _CMPs(Component.ComponentsCls()):
+            led = PoweredLED()
+            power_switch = PowerSwitch(
+                lowside=not logic_low, normally_closed=normally_on
+            )
+
+        self.CMPs = _CMPs(self)
+
+        #
+        self.CMPs.led.IFs.power.connect_via(self.CMPs.power_switch, self.IFs.power_in)
+        self.CMPs.power_switch.IFs.logic_in.connect(self.IFs.logic_in)
+
+class PairTester(Component):
+    def __init__(self, logic_low: bool = False) -> None:
+        super().__init__()
+
+        # interfaces
+        class _IFs(Component.InterfacesCls()):
+            power_in = Power()
+            # TODO logical?
+            wires = times(2, Electrical)
+
+        self.IFs = _IFs(self)
+
+        # components
+        class _CMPs(Component.ComponentsCls()):
+            indicator = LEDIndicator(logic_low=logic_low, normally_on=False)
+
+        self.CMPs = _CMPs(self)
+
+        #
+        self.CMPs.indicator.IFs.power_in.connect(self.IFs.power_in)
+
+        # connect logic high to indicator through wire pair
+        self.IFs.wires[0].connect(
+            self.IFs.power_in.IFs.lv if logic_low else self.IFs.power_in.IFs.hv
+        )
+        self.IFs.wires[1].connect(self.CMPs.indicator.IFs.logic_in)
+
+class Tester(Component):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # interfaces
+        class _IFs(Component.InterfacesCls()):
+            power_in = Power()
+
+        self.IFs = _IFs(self)
+
+        # components
+        class _CMPs(Component.ComponentsCls()):
+            cc1 = PairTester()
+            cc2 = PairTester()
+            sbu1 = PairTester()
+            sbu2 = PairTester()
+            shield = PairTester(logic_low=True)  # in-case connected to gnd
+            # power
+            gnd = times(
+                4, lambda: PairTester(logic_low=True)
+            )  # logic_low to give emarker power
+            vbus = times(4, PairTester)
+            # diffpairs: p, n
+            pair1_rx1 = times(2, PairTester)
+            pair2_rx2 = times(2, PairTester)
+            pair3_tx1 = times(2, PairTester)
+            pair4_tx2 = times(2, PairTester)
+            d1 = times(2, PairTester)
+            d2 = times(2, PairTester)
+            # connectors -------
+            usb_c = times(2, USB_C_Receptacle)
+            rj45 = times(2, RJ45_Receptacle)
+
+        self.CMPs = _CMPs(self)
+
+        # connect power to testers
+        for tester in self.CMPs.get_all():
+            if not isinstance(tester, PairTester):
+                continue
+
+            tester.IFs.power_in.connect(self.IFs.power_in)
+
+        # connect receptacles to testers
+        for i in range(2):
+            def connect_diffpair_to_tester_pair(
+                diffpair: DifferentialPair, testerpair: List[PairTester]
+            ):
+                diffpair.IFs.p.connect(testerpair[0].IFs.wires[i])
+                diffpair.IFs.n.connect(testerpair[1].IFs.wires[i])
+
+            # usb ------
+            usb_i = self.CMPs.usb_c[i].IFs
+
+            usb_i.cc1.connect(self.CMPs.cc1.IFs.wires[i])
+            usb_i.cc2.connect(self.CMPs.cc2.IFs.wires[i])
+            usb_i.sbu1.connect(self.CMPs.sbu1.IFs.wires[i])
+            usb_i.sbu2.connect(self.CMPs.sbu2.IFs.wires[i])
+            usb_i.shield.connect(self.CMPs.shield.IFs.wires[i])
+            # power
+            for cable_if, tester in list(zip(usb_i.gnd, self.CMPs.gnd)) + list(zip(
+                usb_i.vbus, self.CMPs.vbus
+            )):
+                cable_if.connect(tester.IFs.wires[i])
+            # diffpairs
+            connect_diffpair_to_tester_pair(usb_i.rx1, self.CMPs.pair1_rx1)
+            connect_diffpair_to_tester_pair(usb_i.rx2, self.CMPs.pair2_rx2)
+            connect_diffpair_to_tester_pair(usb_i.tx1, self.CMPs.pair3_tx1)
+            connect_diffpair_to_tester_pair(usb_i.tx2, self.CMPs.pair4_tx2)
+            connect_diffpair_to_tester_pair(usb_i.d1, self.CMPs.d1)
+            connect_diffpair_to_tester_pair(usb_i.d2, self.CMPs.d2)
+
+            # rj45 -----
+            for cable_pair, tester_pair in zip(
+                self.CMPs.rj45[i].IFs.twisted_pairs,
+                [
+                    self.CMPs.pair1_rx1,
+                    self.CMPs.pair2_rx2,
+                    self.CMPs.pair3_tx1,
+                    self.CMPs.pair4_tx2,
+                ],
+            ):
+                connect_diffpair_to_tester_pair(cable_pair, tester_pair)
 
 
 class Project(Component):
@@ -48,16 +222,30 @@ class Project(Component):
 
         # components
         class _CMPs(Component.ComponentsCls()):
-            pass
+            tester = Tester()
+            psu = USB_C_PSU()
 
         self.CMPs = _CMPs(self)
 
         # power
+        self.CMPs.tester.IFs.power_in.connect(self.CMPs.psu.IFs.power_out)
 
         # function
 
+        # footprints
+        #TODO
+        #self.CMPs.psu.CMPs.usb.add_trait()
+
+        def get_all(comp: Component):
+            collection = [comp]
+            sub = comp.CMPs.get_all()
+            collection.extend(sub)
+            for s in sub:
+                collection.extend(get_all(s))
+            return collection
+
         # hack footprints
-        for r in self.CMPs.get_all():
+        for r in get_all(self):
             if not r.has_trait(has_footprint):
                 r.add_trait(has_symmetric_footprint_pinmap())
 
@@ -78,4 +266,5 @@ path.write_text(netlist, encoding="utf-8")
 
 from faebryk.exporters.netlist.netlist import render_graph
 
-render_graph(t1_)
+plt = render_graph(t1_)
+plt.show()
